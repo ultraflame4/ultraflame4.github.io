64f6e635095049862b36f2c0563e8018ba62736d
ultraflame4 <ultraflame4@gmail.com>
2021-06-12 22:40:45 +0800

    - added minesweeper, redid some html and css

--- static/js/projectlist.js
+++ static/js/projectlist.js
@@ -2,6 +2,7 @@
 
 function populateList(){
     addRepoToList("OnlineTicTacToe",true)
+    addRepoToList("Minesweeper",true)
     addRepoToList("DiscordBotClient")
     addRepoToList("Rock-Paper-Scissors")
     addRepoToList("PythonQtBrowser")
@@ -18,11 +19,11 @@ function addRepoToList(name,has_page=false){
 
 
 
-    let list_item = document.createElement("li")
-    list_item.setAttribute("class","project-list-item")
+    let list_item = document.createElement("div")
+    list_item.className="project-list-item row"
     list_item.setAttribute("onclick","projectlistItemClicked(this)")
-    list_item.textContent = name
     list_item.dataset.name = name
+    list_item.textContent = name
     list_item.dataset.url = "https://github.com/ultraflame4/"+name
     list_item.dataset.has_site=has_page
 
