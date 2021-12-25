e6363493572e8d9d6aec1f038e35e4bd214cd5a1
ultraflame42 <34125174+ultraflame4@users.noreply.github.com>
2021-12-25 21:34:09 +0800

    did navbar

---
+++ src/components/NavHeader/NavHeader.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="nav-header">
+    <h4 id="title"><a href="/">ultr42</a></h4>
+    <div id="items">
+      <NavHeaderPageLink title="About" link="/#about"/>
+      <NavHeaderPageLink title="Projects" link="/#projects"/>
+      <NavHeaderPageLink title="Contact" link="/#contact"/>
+      <NavHeaderPageLink title="Socials" link="/#socials"/>
+      <NavHeaderPageLink title="Others" link="/#others"/>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+
+import NavHeaderPageLink from "./NavHeaderPageLink.vue";
+export default {
+  name: "Header",
+  components: {NavHeaderPageLink},
+}
+</script>
+
+<style scoped lang="sass">
+@import "@/assets/globals"
+#nav-header
+  @extend .m-0
+  @extend .fw
+  @extend .row-v-center
+  height: $size-8
+  background-color: $color-dark1
+  color: $color-white1
+  display: flex
+
+  #title
+    width: 200px
+
+  #items
+
+    display: flex
+    flex-grow: 1
+    justify-content: flex-end
+    column-gap: $size-1
+    height: 100%
+    padding-right: 100px
+
+
+</style>
---
+++ src/components/NavHeader/NavHeaderPageLink.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="navbaritem-container">
+    <button @click="open_link(link)">
+      <h5>
+        {{title}}
+      </h5>
+    </button>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: "NavHeaderPageLink",
+  props:["title","link"],
+  methods:{
+    open_link(url:string){
+      window.open(url,"_self")
+    }
+  }
+}
+</script>
+
+<style scoped lang="sass">
+@import "@/assets/globals"
+div
+  button
+    height: 70%
+    background-color: rgba($color-white1,0)
+    border: none
+    border-radius: 10px
+    h5
+      color: $color-white2
+    @extend .p-sides-30px
+
+  button:hover
+    background-color: rgba($color-white1,.2)
+
+  button:active
+    background-color: rgba($color-white1,.3)
+
+</style>
