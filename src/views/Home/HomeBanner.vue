63b727cdc84f73449b0cda7b3fc6bd6ff8d53c10
ultraflame42 <34125174+ultraflame4@users.noreply.github.com>
2021-12-27 21:12:28 +0800

    banner arrow fix + compile

--- src/views/Home/HomeBanner.vue
+++ src/views/Home/HomeBanner.vue
@@ -29,7 +29,7 @@ export default {
   components: {SlideshowItem, SlideShow},
   methods: {
     scrolldowb(){
-      window.location.replace("http://localhost:3000/#projects-view")
+      window.location.replace("/#projects-view")
     },
     getImages() {
       //https://wallpapercave.com/wp/wp2077533.jpg,
