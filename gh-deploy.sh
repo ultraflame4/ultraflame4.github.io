0a3aa3d2df213f8d59d4ed0a531b6313059073b9
ultraflame42 <34125174+ultraflame4@users.noreply.github.com>
2022-04-01 15:18:28 +0800

    build

--- gh-deploy.sh
+++
@@ -1,2 +0,0 @@
-echo deploying contents of ./dist folder to gh-pages branch
-git subtree push --prefix dist origin gh-pages
--- src/gvars.ts
+++
@@ -1,5 +0,0 @@
-const gvars={
-    discomode: false,
-    discomode_callback: ()=>{}
-}
-export default gvars
