14b388f87d304698d3eb960e25be7097d5c9f8d7
ultraflame42 <34125174+ultraflame4@users.noreply.github.com>
2022-09-01 20:50:25 +0800

    Added working deploy script

--- README.md
+++ README.md
@@ -4,5 +4,7 @@ This is the repo for version 5 of ultraflame4.github.io site
 ## License
 Source Code: [MIT](LICENSE)
 
+## Deploying
+use [deploy.py](deploy.py) to deploy.
 
 Note to self: https://gist.github.com/belohlavek/61dd16c08cd9c57a168408b9ac4121c2 when deploying.
--- deploy.py
+++ deploy.py
@@ -1,12 +1,31 @@
-BRANCH_NAME=$(git branch --show-current)
-BUILD_DIR="./dist/${BRANCH_NAME}/"
-echo Deploying website! Make sure it has been built! Verifying...
-if [ -d "${BUILD_DIR}" ];then
-  echo
+#! python3
+import os
+import sys
+import shutil
 
-  echo deploying website from branch ${BRANCH_NAME}
-  git subtree push --prefix dist origin deployment
+print("Starting deployment of website")
 
-else
-  echo "Warning could not find build at '${BUILD_DIR}' !. Aborting...!"
-fi
+branch_name = os.popen("git branch --show-current").read().strip()
+deploy_dir = f"builds/{branch_name}"
+gh_pages_branch = "deploy-please"
+
+print(f"Deploying Current Branch ( '{branch_name}' )")
+
+print("Searching for dist folder...")
+if not os.path.exists("dist"):
+    print("dist folder not found")
+    sys.exit(1)
+
+print(f"Copying dist folder to {deploy_dir}")
+
+shutil.copytree("./dist",deploy_dir,dirs_exist_ok=True)
+
+print("Finished copying of dist folder")
+print(os.popen(f'git add "./{deploy_dir}/*"').read())
+print("Commiting...")
+print(os.popen(f"git commit -m \"deploying {branch_name}\"").read())
+
+print("Deploying to github pages...")
+print(os.popen(f"git subtree push --prefix builds origin {gh_pages_branch}").read())
+
+os.system("pause")
