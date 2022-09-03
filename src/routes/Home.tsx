c114d075eccf51688ff85bedee30e3a1f412b51a
ultraflame42 <34125174+ultraflame4@users.noreply.github.com>
2022-09-03 15:02:31 +0800

    Added about section : )

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -24,8 +24,8 @@ export default function Home() {
                 </div>
 
                 <ul id={"header-links"}>
-                    <li><HashLink to={"/#about"}>about</HashLink></li>
-                    <li><HashLink to={"/#projects"}>projects</HashLink></li>
+                    <li><HashLink smooth to={"/#about"}>about</HashLink></li>
+                    <li><HashLink smooth to={"/#projects"}>projects</HashLink></li>
                 </ul>
 
 
@@ -46,10 +46,64 @@ export default function Home() {
             >
 
 
-                <section className={"fillViewport"} id={"about"}>
-                    <UnderConstruction/>
+                <section className={"fillViewport centerItems flex-col"} id={"about"}>
+                    <h2>About</h2>
+                    <p style={{
+                        textAlign: "center"
+                    }}>
+                        I'm an aspiring developer who started programming in 2018-2019.
+                        <br/>
+                        I don't really rmb.
+                        <br/>
+                        learned programming at the age of 12, learning from various
+                        youtube videos & online courses.
+                        <br/>
+                        Since then, I've dabbled in a bunch of stuff and have done a variety projects
+                        <br/>
+                        and also learned a bunch of (programming) languages.
+                    </p>
+                    <br/>
+                    <br/>
+                    {/* todo Maybe add a compentency/preferred meter here */}
+                    <ul id={"about-languages"}>
+                        <li>
+                            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/csharp/csharp-original.svg"
+                                 alt={"C sharp logo"}/>
+                        </li>
+                        <li>
+                            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/unity/unity-original.svg"
+                                 alt={"unity logo"} style={{filter: "invert()"}}/>
+                        </li>
+                        <li>
+
+                            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/java/java-original.svg"
+                                 alt={"java logo"}/>
+                        </li>
+                        <li>
+                            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg"
+                                 alt={"python logo"}/>
+                        </li>
+                        <li>
+                            <img
+                                src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg"
+                                alt={"javascript logo"}/>
+                        </li>
+                        <li>
+                            <img
+                                src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg"
+                                alt={"typescript logo"}/>
+                        </li>
+                        <li>
+                            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vuejs/vuejs-original.svg"
+                                 alt={"vue.js logo"}/>
+                        </li>
+                        <li>
+                            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg"
+                                 alt={"react.js logo"}/>
+                        </li>
+                    </ul>
                 </section>
-                <section className={"fillViewport"}>
+                <section className={"fillViewport"} id={"#projects"}>
                     <UnderConstruction/>
                 </section>
             </main>
