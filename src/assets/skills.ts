fe1475ec440ccfa48a5f0582a1651f9ba3771874
ultraflame42 <34125174+ultraflame4@users.noreply.github.com>
2022-11-07 18:16:25 +0800

    Moved skills to a seperate file, added css and html to skills list

---
+++ src/assets/skills.ts
@@ -0,0 +1,61 @@
+interface p_language {
+    name: string,
+    logo: string,
+    confidence: number // percentage.
+    invert_image?: boolean
+}
+
+
+export const skills: p_language[] = [
+    {
+        name: "C Sharp",
+        logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/csharp/csharp-original.svg",
+        confidence: .7
+    },
+    {
+        name: "Unity",
+        logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/unity/unity-original.svg",
+        confidence: .7,
+        invert_image: true
+    },
+    {
+        name: "Java",
+        logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/java/java-original.svg",
+        confidence: .6
+    },
+    {
+        name: "Python",
+        logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg",
+        confidence: .9
+    },
+    {
+        name: "HTML 5",
+        logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg",
+        confidence: .65
+    },
+    {
+        name: "CSS 3",
+        logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg",
+        confidence: .65
+    },
+    {
+        name: "Javascript",
+        logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg",
+        confidence: .75
+    },
+    {
+        name: "Typescript",
+        logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg",
+        confidence: .8
+    },
+    {
+        name: "Vue.js",
+        logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vuejs/vuejs-original.svg",
+        confidence: .7
+    },
+    {
+        name: "React.js",
+        logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg",
+        confidence: .8
+    }
+]
--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -7,58 +7,7 @@ import Counter from "../components/Counter";
 import FunnyOverlay from "../components/easter_eggs/LangEasterEggOverlay";
 import HashLinkW from "../components/HashLinkW";
 import {PageLinkData} from "@/tools";
-
-
-interface p_language {
-    name: string,
-    logo: string,
-    confidence: number // percentage.
-    invert_image?: boolean
-}
-
-const skills: p_language[] = [
-    {
-        name: "C Sharp",
-        logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/csharp/csharp-original.svg",
-        confidence: .7
-    },
-    {
-        name: "Unity",
-        logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/unity/unity-original.svg",
-        confidence: .7,
-        invert_image: true
-    },
-    {
-        name: "Java",
-        logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/java/java-original.svg",
-        confidence: .6
-    },
-    {
-        name: "Python",
-        logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg",
-        confidence: .9
-    },
-    {
-        name: "Javascript",
-        logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg",
-        confidence: .75
-    },
-    {
-        name: "Typescript",
-        logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg",
-        confidence: .8
-    },
-    {
-        name: "Vue.js",
-        logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vuejs/vuejs-original.svg",
-        confidence: .7
-    },
-    {
-        name: "React.js",
-        logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg",
-        confidence: .8
-    }
-]
+import {skills} from "@/assets/skills";
 
 
 const HeaderLinksList: PageLinkData[] = [
