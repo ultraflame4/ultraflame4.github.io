dc67bcb2f426afbba4d43cd6010c74581e28c536
ultraflame42 <34125174+ultraflame4@users.noreply.github.com>
2023-04-01 14:07:50 +0800

    updated colors

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,20 @@
 <template>
+<main>
+  <h1>
+    Title
+  </h1>
+  <h2>
+    Title 2
+  </h2>
+  <p>
+    Paragraph PWJIANHFPIUAWHBNF<br/>
+    Paragraph PWJIANHFPIUAWHBNF <a>Links</a> PWJIANHFPIUAWHBNF <br/>
+    Paragraph PWJIANHFPIUAWHBNF<br/>
 
+  </p>
+  <a>Links</a><br>
+  <a>Links</a>
+</main>
 </template>
 
 <script lang="ts" setup>
