bed199b3794b2c95a227b31346dc9b4ecbf2250f
ultraflame42 <34125174+ultraflame4@users.noreply.github.com>
2023-04-01 21:05:02 +0800

    starting on top nav

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,17 @@
 <template>
-  <br/>
-  <div id="header-ctn" v-stuck="headerStuck">
-    <header>
-      <h1>ultr42</h1>
-      {{headerStuck}}
-    </header>
-  </div>
 
+  <TopNav/>
   <div id="page-content">
 
     <RouterView/>
   </div>
 </template>
 <script setup lang="ts">
-import {RouterView} from 'vue-router'
-import {ref} from "vue";
-const headerStuck = ref({stuck:false})
 
+import TopNav from "@/components/TopNav.vue";
 </script>
 
 <style lang="scss" scoped>
-#header-ctn{
-  padding: 0 1rem;
-  position: sticky;
-  top: 0;
-  height: 8rem;
-  display: flex;
-  align-items: center;
-}
-#header-ctn[stuck] {
-  background: white;
-}
-header {
-  padding: 1rem;
-  width: 100%;
-
-  &> *{
-    margin: 0;
-  }
-}
 
 #page-content {
   padding-top: 5rem;
---
+++ src/components/TopNav.vue
@@ -0,0 +1,59 @@
+<template>
+  <br/>
+  <div id="header-ctn" v-stuck>
+    <header>
+      <h1>ultr42</h1>
+
+    </header>
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style lang="scss" scoped>
+#header-ctn {
+
+  position: sticky;
+  top: 0;
+  height: 8rem;
+  pointer-events: none;
+}
+
+header {
+  height: 100%;
+  width: 100%;
+  transition: all 200ms ease;
+  display: flex;
+  align-items: center;
+  padding: 0 2rem;
+  pointer-events: all;
+  & > h1{
+    transition: all 200ms ease;
+    font-family: "Open sans";
+    font-weight: 700;
+    font-size: 1.75rem;
+    letter-spacing: 0.2rem;
+  }
+
+  & > * {
+    margin: 0;
+  }
+}
+
+#header-ctn[stuck] > header {
+  height: 4rem;
+
+
+  & > h1{
+    font-size: 1.25rem;
+  }
+}
+
+#header-ctn[stuck]{
+  background: linear-gradient(180deg, var(--bg-0) 40%, transparent);
+}
+
+</style>
+6
