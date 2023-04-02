dfda79bac0ccfaffbb2a9deec2557b55e6e70fa2
ultraflame42 <34125174+ultraflame4@users.noreply.github.com>
2023-04-02 13:06:26 +0800

    added menu hamburger and future space for links behind the site

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,78 @@
 <template>
+  <div id="site-back">
 
-  <TopNav/>
-  <div id="page-content">
-
-    <RouterView/>
   </div>
+  <div id="site-ctn" :data-open="menuOpen" @click.capture="returnToPage()">
+    <TopNav :menu-open="menuOpen" @menuToggle="menuToggle()"/>
+    <div id="page-content">
+      <RouterView/>
+    </div>
+  </div>
+
 </template>
 <script setup lang="ts">
-
 import TopNav from "@/components/TopNav.vue";
+import {ref} from "vue";
+
+const menuOpen = ref(false);
+const isClosing = ref(false);
+
+function returnToPage(){
+  if (menuOpen.value) {
+    menuOpen.value=false
+    isClosing.value=true
+    setTimeout(()=> {isClosing.value = false},0) // clean up
+  }
+}
+
+function menuToggle() {
+  // no need to deal with closing of menu because it will be closed by the returnToPage function
+  menuOpen.value=!isClosing.value
+}
+
 </script>
 
 <style lang="scss" scoped>
+#site-back {
+  position: fixed;
+  top: 0;
+  left: 0;
+  height: 100vh;
+  width: 100vw;
+  background: var(--bg-1);
+  z-index: -1;
+}
+#site-ctn {
+  margin: 0;
+  transition: all 500ms ease;
+  border: 2px solid transparent;
+  width: 100%;
+  height: 100vh;
+  overflow: auto;
+  background: var(--bg-0);
+
+}
+#site-ctn[data-open="true"] {
+  border-color: var(--bg-2);
+  @media only screen and (max-width: 800px) {
+    transform: translateX(-40vw) translateY(3vh);
+  }
+  @media only screen and (min-width: 801px) {
+    margin-top: 3%;
+    width: calc(100% - 20rem);
+  }
+
+
+
+
+  border-top-right-radius: 1rem;
+}
+#site-ctn::-webkit-scrollbar-track{
+  margin-top: 1rem;
+}
 
 #page-content {
-  padding-top: 5rem;
+  padding-top: 1rem;
 }
 
 </style>
--- src/components/TopNav.vue
+++ src/components/TopNav.vue
@@ -3,22 +3,59 @@
   <div id="header-ctn" v-stuck>
     <header>
       <h1>ultr42</h1>
-
+      <button id="menu-btn" @click="emit('menuToggle')" :data-open="menuOpen">
+        <Icon icon="ic:baseline-menu"/>
+      </button>
     </header>
   </div>
 </template>
 
 <script lang="ts" setup>
+import {Icon} from "@iconify/vue";
+import {type Ref} from "vue";
+
+const emit = defineEmits<{
+  (e: 'menuToggle'): void
+}>()
+
+const props = defineProps<{
+  menuOpen:boolean
+}>()
+
 
 </script>
 
 <style lang="scss" scoped>
+
+#menu-btn {
+  background: none;
+  border: none;
+  color: white;
+  height: fit-content;
+  display: flex;
+  align-items: center;
+  width: fit-content;
+  font-size: 1.3em;
+  transition: color 200ms ease, transform 100ms linear;
+  margin-left: auto;
+}
+
+#menu-btn:hover, #menu-btn[data-open="true"]{
+  color: var(--accent);
+}
+
+#menu-btn:active{
+  transform: scale(0.8);
+}
+
 #header-ctn {
 
   position: sticky;
-  top: 0;
+
   height: 8rem;
   pointer-events: none;
+  font-size: 1.6rem;
+  top: -1px;
 }
 
 header {
@@ -27,14 +64,16 @@ header {
   transition: all 200ms ease;
   display: flex;
   align-items: center;
-  padding: 0 2rem;
+  padding: 0 1rem;
   pointer-events: all;
+
   & > h1{
+    margin-left: 3vw;
     transition: all 200ms ease;
-    font-family: "Open sans";
+    font-family: "Poppins";
     font-weight: 700;
-    font-size: 1.75rem;
-    letter-spacing: 0.2rem;
+    font-size: 1em;
+    letter-spacing: 0.1rem;
   }
 
   & > * {
@@ -44,16 +83,14 @@ header {
 
 #header-ctn[stuck] > header {
   height: 4rem;
+  padding: 0;
 
-
-  & > h1{
-    font-size: 1.25rem;
-  }
 }
 
 #header-ctn[stuck]{
-  background: linear-gradient(180deg, var(--bg-0) 40%, transparent);
+  font-size: 1.25rem;
+  background: linear-gradient(180deg, var(--bg-0) 4rem, transparent 6rem);
 }
 
 </style>
-6
+
