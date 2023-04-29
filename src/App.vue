9b18994a3de6f2fc89c1dddc25721992a43b2f0a
ultraflame42 <34125174+ultraflame4@users.noreply.github.com>
2023-04-30 00:13:33 +0800

    added this page links to the full menu

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
     <div id="site-back">
-
+        <NavigationTree id="navtree"/>
     </div>
     <div id="site-ctn" :data-open="menuOpen" @click.capture="returnToPage()">
         <TopNav :menu-open="menuOpen" @menuToggle="menuToggle()"/>
@@ -13,6 +13,7 @@
 <script setup lang="ts">
 import TopNav from "@/components/TopNav.vue";
 import {ref} from "vue";
+import NavigationTree from "@/components/NavigationTree.vue";
 
 const menuOpen = ref(false);
 const isClosing = ref(false);
@@ -35,44 +36,55 @@ function menuToggle() {
 </script>
 
 <style lang="scss" scoped>
+
+#site-back{
+    display: flex;
+}
+#navtree{
+    margin-top: 1rem;
+    margin-left: auto;
+}
+
 #site-back {
-  position: fixed;
-  top: 0;
-  left: 0;
-  height: 100vh;
-  width: 100vw;
-  background: var(--bg-1);
-  z-index: -1;
+    position: fixed;
+    top: 0;
+    left: 0;
+    height: 100vh;
+    width: 100vw;
+    background: var(--bg-1);
+
 }
 
 #site-ctn {
-  margin: 0;
-  transition: all 500ms ease;
-  border: 2px solid transparent;
-  width: 100%;
-  height: 100vh;
-  overflow: auto;
-  background: var(--bg-0);
+    position: fixed;
+    z-index: 1;
+    margin: 0;
+    transition: all 500ms ease;
+    border: 2px solid transparent;
+    width: 100%;
+    height: 100vh;
+    overflow: auto;
+    background: var(--bg-0);
 
 }
 
 #site-ctn[data-open="true"] {
-  border-color: var(--bg-2);
+    border-color: var(--bg-2);
 
-  margin-left: 2rem;
-  width: calc(100% - min(30vw, 20rem));
-  height: calc(100% - 3rem);
-  filter: drop-shadow(3px 3px 1rem var(--bg-0));
-  border-radius: 1rem;
+    margin-left: 2rem;
+    width: calc(100% - min(30vw, 20rem));
+    height: calc(100% - 3rem);
+    filter: drop-shadow(3px 3px 1rem var(--bg-0));
+    border-radius: 1rem;
     transform: translateX(-1rem) translateY(2rem);
 }
 
 #site-ctn::-webkit-scrollbar-track {
-  margin-top: 1rem;
+    margin-top: 1rem;
 }
 
 #page-content {
-  padding-top: 1rem;
+    padding-top: 1rem;
 }
 
 </style>
---
+++ src/components/NavigationTree.vue
@@ -0,0 +1,54 @@
+<template>
+    <div>
+        <h1>This Page</h1>
+        <ul id="thispage-list">
+            <li v-for="link in PageNavTree.links.value">
+                <Icon icon="vaadin:hash" :inline="true" class="icon"/>
+                <RouterLink :to="link.to" class="parent_hover-underline no-hover">{{link.name}}</RouterLink>
+
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script lang="ts" setup>
+
+import {PageNavTree} from "@/router/page_navtree";
+import {Icon} from "@iconify/vue";
+</script>
+
+<style lang="scss" scoped>
+div{
+    padding: 0.5em 1rem;
+    padding-left: 0;
+}
+h1{
+    font-family: "Poppins";
+    letter-spacing: 0.2rem;
+    text-transform: uppercase;
+    font-size: 2.25rem;
+}
+ul#thispage-list{
+    list-style-type: none;
+    padding-left: 0;
+    --level:0;
+    & > li{
+        font-size: calc(1.45em - (var(--level) * 10%));
+        padding-left: calc(var(--level) * 1em);
+        height: 2em;
+
+        .icon{
+            color: var(--accent);
+        }
+
+        a{
+            font-family: "Poppins";
+            --underline-mult: 1;
+            font-weight: 700;
+            letter-spacing: 0.05rem;
+            text-transform: uppercase;
+            text-decoration: none;
+        }
+    }
+}
+</style>
