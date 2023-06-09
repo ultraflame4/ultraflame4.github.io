2fc31b6aeb1da9de4b7ff67dc8744b93ea1fa888
ultraflame42 <34125174+ultraflame4@users.noreply.github.com>
2023-06-09 17:53:04 +0800

    added bg

--- src/components/page/SectionTitle.vue
+++ src/components/page/SectionTitle.vue
@@ -1,5 +1,5 @@
 <template>
-    <TopSticky >
+    <TopSticky>
         <h1 class="no-vmar">
             <HashLink to="featured" :heading="0"><slot></slot></HashLink>
         </h1>
@@ -15,10 +15,13 @@ import HashLink from "@/components/core/HashLink.vue";
 </script>
 
 <style lang="scss" scoped>
+h1{
+    background-color: var(--bg-0);
+}
 [stuck] > div > h1{
-    //color: var(--txt-a-tinted);
+    color: var(--txt-a-tinted);
     font-size: 1em;
     position: relative;
-
+    top: 1px;
 }
 </style>
