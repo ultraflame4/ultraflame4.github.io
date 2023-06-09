928c3ef7e08eef6feb802e38d07555a873a9c7e3
ultraflame42 <34125174+ultraflame4@users.noreply.github.com>
2023-06-09 22:09:27 +0800

    changed to webhash history cuz github pages will 404 on a subpage on reload

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,10 @@
-import {createRouter, createWebHistory} from 'vue-router'
+import {createRouter, createWebHashHistory, createWebHistory} from 'vue-router'
 import Home from "@/views/Home.vue";
 import {PageNavTree} from "@/router/page_navtree";
 import Projects from "@/views/Projects.vue";
 
 const router = createRouter({
-    history: createWebHistory(import.meta.env.BASE_URL),
+    history: createWebHashHistory(import.meta.env.BASE_URL),
     routes: [
         {
             path: '/',
