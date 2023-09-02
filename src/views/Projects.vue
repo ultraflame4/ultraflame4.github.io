33078000e10b2b7ccd80e7eb29b02075f265cf87
ultr42 <ultr42@skiff.com>
2023-09-02 18:24:49 +0800

    Added noscript and other info text

--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -9,8 +9,19 @@
                 <p>Fetching project data</p>
                 <LoadingSpinner/>
             </div>
-            <p v-if="ProjectDataStatus.isfallback" class="text-warn">
-                Could not get latest project data. Using fallback data!
+            <noscript>
+                <p class="text-danger text-center">
+                    Javascript is disabled
+                    <br/>
+                    It is needed to fetch the latest project data & load the fallback data
+                </p>
+            </noscript>
+            <p v-if="ProjectDataStatus.isfallback" class="text-warn text-center">
+                Blocked? Outage? Offline? Who knows?
+                <br/>
+                Unable to fetch latest project data! Using (Outdated) fallback data!
+                <br/>
+                Server Unreachable
             </p>
             <ul v-if="!ProjectDataStatus.loading.value" id="projects-container">
                 <li v-for="(p, index) in searchResults" :key="index">
@@ -102,11 +113,12 @@ watch([searchTerm, AllProjects], ([search_term, _]) => {
     align-items: center;
     justify-content: center;
     flex-direction: column;
+
     & > p {
         margin: 1rem 0;
         letter-spacing: 1px;
         text-transform: uppercase;
-        font-family: Montserrat,serif;
+        font-family: Montserrat, serif;
         font-weight: 600;
         color: var(--bg-3);
     }
