6911d7e3c15ca3e4c227c4c8e3ec25254ebfbdd4
ultr42 <ultr42@skiff.com>
2023-11-12 13:58:05 +0800

    updated stuff

--- src/components/content/Projects/GetProjectLink.vue
+++ src/components/content/Projects/GetProjectLink.vue
@@ -1,13 +1,20 @@
 <template>
-    <ProjectDefaultLink/>
+
+    <ProjectReIconLink v-if="value.url?.match(/https:\/\/github.com\/.*\/releases($|\/.*)/g)"
+                       icon="ph:archive-box-fill"/>
+    <ProjectReIconLink v-else-if="value.url?.match(/https:\/\/.*\.itch\.io\/.*/g)" icon="simple-icons:itchdotio"/>
+    <ProjectDefaultLink v-else/>
+
 </template>
 <script setup lang="ts">
 import type {proj_entry_link} from "@/assets/projects";
 import {type InjectionKey, provide} from "vue";
 import ProjectDefaultLink from "@/components/content/Projects/ProjectDefaultLink.vue";
-const props = defineProps<{value:proj_entry_link}>()
+import ProjectReIconLink from "@/components/content/Projects/ProjectReIconLink.vue";
+
+const props = defineProps<{ value: proj_entry_link }>()
 const key = Symbol() as InjectionKey<string>
-provide("link",props.value)
+provide("link", props.value)
 
 </script>
 <style scoped lang="scss">
---
+++ src/components/content/Projects/ProjectReIconLink.vue
@@ -0,0 +1,24 @@
+<template>
+    <ProjectDefaultLink/>
+</template>
+<script setup lang="ts">
+import {inject, provide} from "vue";
+import type {proj_entry_link} from "@/assets/projects";
+import ProjectDefaultLink from "@/components/content/Projects/ProjectDefaultLink.vue";
+
+interface props_{
+    icon: string
+}
+
+const props = defineProps<props_>();
+const value = inject("link") as proj_entry_link
+value.icon = props.icon
+const value_ = provide("link", value)
+
+
+</script>
+<style scoped lang="scss">
+iframe{
+    //transform: scale(0.5);
+}
+</style>
