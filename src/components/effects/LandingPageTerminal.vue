610767b638ba58c44fb211c9bff5361b3f3d8aa4
ultr42 <ultraflame4@gmail.com>
2025-05-15 22:32:41 +0800

    adjusted terminal sizing + timing

--- src/components/effects/LandingPageTerminal.vue
+++ src/components/effects/LandingPageTerminal.vue
@@ -41,22 +41,22 @@ const terminalInstructions: TerminalTextInstruction[] = [
     tti_delay(10),
     tti("Starting super awesome program! Prepare for awesomeness. 🎉🎉🎉🎉🎉"),
     tti(helloWorld),
-    tti_delay(100),
+    tti_delay(90),
     tti("Ok now, you can scroll down."),
-    tti_delay(200),
+    tti_delay(100),
     tti("Come on, scroll down, theres more stuff below."),
-    tti_delay(300),
+    tti_delay(200),
     tti("Why are you still here, scroll down, please."),
-    tti_delay(400),
+    tti_delay(300),
     tti("Seriously theres, scroll down."),
-    tti_delay(500),
-    tti("Ok whatever you can stay here if you like...."),
+    tti_delay(400),
+    tti("Ok whatever, you can stay here if you like...."),
     
 ]
 
 </script>
 <template>
-    <FakeTerminal :sequence="terminalInstructions">
+    <FakeTerminal :sequence="terminalInstructions" style="font-size:  min(1rem, 1.2vw);">
 
     </FakeTerminal>
 </template>
