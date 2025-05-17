798b1879b4591da25c847dd6816b9bd8a00a0888
ultr42 <ultraflame4@gmail.com>
2025-05-17 14:40:15 +0800

    aaaa + complete refactor of css theme variables n stuff

---
+++ src/components/ui/EditorLikeCard.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="root">
+        <h1></h1>
+        <div>
+
+        </div>
+    </div>
+</template>
+<script lang="ts" setup>
+
+</script>
+<style scoped>
+.root {}
+</style>
---
+++ src/components/ui/index.ts
@@ -0,0 +1,6 @@
+import EditorLikeCard from "./EditorLikeCard.vue";
+
+/**
+ * A Card / Section that inspired by an editor
+ */
+export const EdCard = EditorLikeCard;
