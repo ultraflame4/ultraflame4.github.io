276c45c2e3624a6a6202b9863505ee87bd54a893
ultr42 <ultraflame4@gmail.com>
2025-05-18 10:55:54 +0800

    new component stub

---
+++ src/components/ui/WindowBook.vue
@@ -0,0 +1,9 @@
+<template>
+    <!-- This component is to render a window -->
+    <!-- Sidebar contains tabs to each chapter -->
+    <!-- Content of window should render markdown -->
+</template>
+<script lang="ts" setup>
+
+</script>
+<style></style>
