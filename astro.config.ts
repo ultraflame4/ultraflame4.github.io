5ec4257818113ea731938bdbbb95f05923c02359
ultr42 <ultraflame4@gmail.com>
2025-06-21 12:55:56 +0800

    add site version

--- astro.config.ts
+++ astro.config.ts
@@ -2,16 +2,34 @@
 import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
-
 import vue from '@astrojs/vue';
 import remarkDirective from 'remark-directive';
 import { remarkIconDirective } from './src/meta/remark-plugins';
+import { version } from "./package.json"
+
+import * as childProcess from "child_process";
+
+let commitHash = ""
+try {
+    commitHash = childProcess
+        .execSync('git rev-parse --short HEAD')
+        .toString();
+    console.log("App at commit hash:", commitHash)
+}
+catch (e) {
+    console.warn("Failed to fetch commit hash! Are we in a non-git environment?")
+}
+
 
 
 // https://astro.build/config
 export default defineConfig({
     vite: {
         plugins: [tailwindcss()],
+        define: {
+            'import.meta.env.__APP_VERSION__': JSON.stringify(version),
+            'import.meta.env.__COMMIT_HASH__': JSON.stringify(commitHash),
+        }
     },
     markdown: {
         remarkPlugins: [
