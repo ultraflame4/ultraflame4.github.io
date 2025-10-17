bc7d469efc2beb86e759038d75fdbb4c5402a860
ultr42 <ultraflame4@gmail.com>
2025-10-17 23:12:55 +0800

    update

--- src/components/ui/RSSFeed/RSSFeedViewer.vue
+++ src/components/ui/RSSFeed/RSSFeedViewer.vue
@@ -1,6 +1,7 @@
 <template>
     <div class="p-4">
         <div class="bg-highlight p-[1px] flex flex-col gap-[1px]">
+            <p v-if="loading" class="p-4 bg-background">Loading...</p>
             <FeedItem v-for="item in items" :item="item"></FeedItem>
         </div>
     </div>
@@ -17,9 +18,12 @@ const props = defineProps<{
     max_items?: number
 }>()
 const items = ref<htmlparser2.DomUtils.FeedItem[]>([])
+const loading = ref(true)
 
 onMounted(async () => {
+    loading.value = true;
     const res = await fetch(props.feedurl)
+    loading.value = false;
     const feed = htmlparser2.parseFeed(await res.text());
     if (feed == null) return;
     items.value = feed.items.slice(0, props.max_items ?? 10)
