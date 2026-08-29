c3f152d28c9220a032be0feb3d080d4d9cedbe92
ultr42 <ultraflame4@gmail.com>
2026-08-29 18:58:29 +0800

    Creating nav bar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,4 +6,9 @@
 </script>
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
+<header class="p-(--nav-offset-units) w-full flex items-center justify-center">
+    <nav class="acrylic h-(--nav-height-units) rounded-2xl min-w-32 w-fit">
+        
+    </nav>
+</header>
 {@render children()}
