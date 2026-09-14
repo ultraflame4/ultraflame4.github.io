d672a6b0674046fad41ef967ab641623741b10aa
ultraflame4 <ultraflame4@gmail.com>
2026-09-14 17:59:42 +0800

    Attempt infer schema

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -1,9 +1,13 @@
+import projectSchema from "../../data/projects/schema.json";
+import {type FromSchema} from "json-schema-to-ts"
 import { minimatch } from "minimatch";
 
 const full_data = import.meta.glob("../../data/**/*", {
     query: "?raw",
     eager: true
 });
+
+type ProjectEntry =FromSchema<typeof projectSchema>
 
 export function readContent(
     folder: string,
